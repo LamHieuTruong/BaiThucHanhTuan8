39c96e9b864f2bbb0298a7bcd674f181438fa7a0
LAM <lamle28082001@gmail.com>
2022-04-18 14:53:04 +0700

    first commit

---
+++ app/src/main/java/com/example/th_tuan8/GiaoDien.java
@@ -0,0 +1,14 @@
+package com.example.th_tuan8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GiaoDien extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_giao_dien);
+    }
+}
---
+++ app/src/main/java/com/example/th_tuan8/GiaoDienDangNhap.java
@@ -0,0 +1,14 @@
+package com.example.th_tuan8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GiaoDienDangNhap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_giao_dien_dang_nhap);
+    }
+}
